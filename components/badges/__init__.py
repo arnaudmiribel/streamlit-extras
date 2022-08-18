ac456b62a4638206c7f9fe7f11743a26bd461e68
Arnaud 🎈 <arnaud@streamlit.io>
2022-08-18 16:50:17 +0200

    Badges

---
+++ components/badges/__init__.py
@@ -0,0 +1,7 @@
+from . import main
+
+__func__ = main.badge
+__title__ = "Badges"
+__desc__ = "Create custom badges (e.g. PyPI, Streamlit Cloud)"
+__icon__ = "🏷️"
+__examples__ = [main.example_pypi, main.example_streamlit, main.example_github]
