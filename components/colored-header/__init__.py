c8740aa6f9e958db8c0c44fa117dac9b6c48daba
tyler.richards <tyler.richards@snowflake.com>
2022-08-19 10:27:40 -0700

    changes for pr

--- components/colored-header/__init__.py
+++ components/colored-header/__init__.py
@@ -5,4 +5,4 @@ __title__ = "Color ya Headers"
 __desc__ = "This function makes headers much prettier in Streamlit"
 __icon__ = "🖌️"
 __examples__ = [main.example]
-__author__ = "Tyler Richards"
+__author__ = "Johannes Rieke / Tyler Richards"
