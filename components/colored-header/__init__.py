9b3c32ee8be80998348eb443bc367ab987c1e325
tyler.richards <tyler.richards@snowflake.com>
2022-08-19 10:08:15 -0700

    edit for colored header

---
+++ components/colored-header/__init__.py
@@ -0,0 +1,8 @@
+from . import main
+
+__func__ = main.colored_header
+__title__ = "Color ya Headers"
+__desc__ = "This function makes headers much prettier in Streamlit"
+__icon__ = "🖌️"
+__examples__ = [main.example]
+__author__ = "Tyler Richards"
