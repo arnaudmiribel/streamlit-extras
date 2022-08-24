71f82b19f46ad1859fea00244851a337140043e0
Arnaud 🎈 <arnaud@streamlit.io>
2022-08-24 15:59:29 +0200

    Add setups

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="streamlit_extras",
+    packages=["streamlit_extras"],
+    version="v0.0.1",  # Ideally should be same as your GitHub release tag varsion
+    description="MVP is out!",
+    author="Arnaud Miribel",
+    author_email="arnaudmiribel@gmail.com",
+    url="https://github.com/arnaudmiribel/streamlit-extras",
+    download_url="https://github.com/arnaudmiribel/streamlit-extras/archive/refs/tags/v0.0.1.tar.gz",
+    keywords=["streamlit", "ui", "python", "data"],
+    classifiers=[],
+)
