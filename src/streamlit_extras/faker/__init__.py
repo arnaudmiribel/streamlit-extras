d967468d7e389497bf28fc0473664a3e459933c3
Arnaud 🎈 <arnaudmiribel@gmail.com>
2022-11-09 15:36:59 +0100

    Update example

--- src/streamlit_extras/faker/__init__.py
+++ src/streamlit_extras/faker/__init__.py
@@ -2,12 +2,12 @@ from streamlit_faker import get_streamlit_faker
 
 
 def example():
-    fake = get_streamlit_faker()
+    fake = get_streamlit_faker(seed=42)
     fake.markdown()
-    fake.markdown()
-    fake.write()
+    fake.info(icon="💡", body="You can also pass explicit parameters!")
     fake.selectbox()
     fake.slider()
+    fake.metric()
     fake.altair_chart()
 
 
@@ -17,4 +17,7 @@ __desc__ = "Fake Streamlit commands at the speed of light! Great for prototyping
 __icon__ = "🥷"
 __examples__ = [example]
 __author__ = "Arnaud Miribel"
+__github_repo__ = "arnaudmiribel/streamlit-faker"
+__pypi_name__ = "streamlit-faker"
+__package_name__ = "streamlit_faker"
 __experimental_playground__ = False
