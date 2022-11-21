6b47da499835b0ec2e0ef562c9d29ee3a3cec610
Gamliel Cohen <gamliel.cohen-ext@pernod-ricard.com>
2022-11-21 16:22:29 +0100

    Added a new extra, st-card. A Streamlit UI Card component

---
+++ src/streamlit_extras/card/__init__.py
@@ -0,0 +1,20 @@
+from streamlit_card import card
+from .. import extra
+
+card = extra(card)
+
+def example():
+    card(
+        title="Hello World!",
+        text="Some description",
+        image="http://placekitten.com/300/250"
+    )
+
+__title__ = "Streamlit Card"  # title of your extra!
+__desc__ = "Streamlit Component, for a UI card"  # description of your extra!
+__icon__ = "🖼️"  # give your extra an icon!
+__examples__ = [example]  # create some examples to show how cool your extra is!
+__author__ = "Gamliel Cohen <gamcoh>"
+__pypi_name__ = "streamlit-card"
+__package_name__ = "streamlit_card"
+__github_repo__ = "gamcoh/st-card" # Optional
