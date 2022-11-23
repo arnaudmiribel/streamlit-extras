ada514b59823136377daa0f75d9689a80dcaf2ce
Gamliel Cohen <gamliel.cohen-ext@pernod-ricard.com>
2022-11-23 11:30:13 +0100

    fix(review): fixed the asked modifications from the PR

--- src/streamlit_extras/card/__init__.py
+++ src/streamlit_extras/card/__init__.py
@@ -1,4 +1,5 @@
 from streamlit_card import card
+
 from .. import extra
 
 card = extra(card)
@@ -7,12 +8,13 @@ def example():
     card(
         title="Hello World!",
         text="Some description",
-        image="http://placekitten.com/300/250"
+        image="http://placekitten.com/300/250",
+        url="https://www.google.com",
     )
 
-__title__ = "Streamlit Card"  # title of your extra!
+__title__ = "Card"  # title of your extra!
 __desc__ = "Streamlit Component, for a UI card"  # description of your extra!
-__icon__ = "🖼️"  # give your extra an icon!
+__icon__ = "💳️"  # give your extra an icon!
 __examples__ = [example]  # create some examples to show how cool your extra is!
 __author__ = "Gamliel Cohen <gamcoh>"
 __pypi_name__ = "streamlit-card"
