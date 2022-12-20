1e725589f5463cc4ee9172491d1b06a579933f67
Shruti Agarwal <82811717+ShruAgarwal@users.noreply.github.com>
2022-12-20 19:00:33 +0530

    Create __init__.py

---
+++ src/streamlit_extras/st_book/__init__.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import streamlit_book as st_book
+from .. import extra
+
+st_book = extra(st_book)
+
+def example():
+    st_book.set_chapter_config(path="pages\chapter")
+    
+
+__title__ = "Streamlit Book - Multipaging Demo"  
+__desc__ = "A companion library to create a interactive reader for the content on a given folder"
+__icon__ = "📙"
+__examples__ = [example]
+__author__ = "Sebastian Flores"
+__pypi_name__ = "streamlit-book"
+__package_name__ = "streamlit_book"
+__github_repo__ = "sebastiandres/streamlit_book"
+__forum_url__ = "https://discuss.streamlit.io/t/happy-birds-showcasing-the-streamlit-book-library/20012" # Optional
+__experimental_playground__ = False # Optional
