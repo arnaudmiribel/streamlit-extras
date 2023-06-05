7542c61492d3c5475b04c29d060463a144b2c4ac
Asaurus1 <alexandermartin2018@u.northwestern.edu>
2023-06-05 12:33:22 -0700

    Fix LOGGER import in streamlit_patches.py

--- gallery/streamlit_patches.py
+++ gallery/streamlit_patches.py
@@ -14,7 +14,7 @@ from streamlit import (
 from streamlit.commands.page_config import get_random_emoji
 from streamlit.runtime.scriptrunner import get_script_run_ctx as _get_script_run_ctx
 from streamlit.runtime.scriptrunner.script_runner import (
-    LOGGER,
+    _LOGGER as LOGGER,
     SCRIPT_RUN_WITHOUT_ERRORS_KEY,
     ForwardMsg,
     RerunData,
