16b13e35c64adc2581cd71a8e946f4d18173aafc
Asaurus1 <alexandermartin2018@u.northwestern.edu>
2023-06-07 00:36:26 -0700

    Update streamlit-qs to v0.1.1 which fixes some issues with the original sdist of 0.1.0

--- src/streamlit_extras/query_string/__init__.py
+++ src/streamlit_extras/query_string/__init__.py
@@ -13,7 +13,28 @@ from streamlit_qs import (
     unenumifier,
     from_query_args,
     from_query_args_index,
+    blacklist_key,
+    unblacklist_key,
 )
+
+from .. import extra
+
+selectbox_qs = extra(selectbox_qs)
+multiselect_qs = extra(multiselect_qs)
+radio_qs = extra(radio_qs)
+text_area_qs = extra(text_area_qs)
+text_input_qs = extra(text_input_qs)
+number_input_qs = extra(number_input_qs)
+checkbox_qs = extra(checkbox_qs)
+make_query_string = extra(make_query_string)
+update_qs_callback = extra(update_qs_callback)
+add_qs_callback = extra(add_qs_callback)
+clear_qs_callback = extra(clear_qs_callback)
+unenumifier = extra(unenumifier)
+from_query_args = extra(from_query_args)
+from_query_args_index = extra(from_query_args_index)
+blacklist_key = extra(blacklist_key)
+unblacklist_key = extra(unblacklist_key)
 
 # EXAMPLES -------------------------------------------------------------------
 def example_text_input():
@@ -72,7 +93,7 @@ def example_permalink():
 
 
 __title__ = "Query String"
-__desc__ = "Utilities and Elements for interacting with the URL query string"
+__desc__ = "Utilities and Elements for interacting with the URL query string. Full example at  \n❓[query-string.streamlit.app](https://query-string.streamlit.app/)"
 __icon__ = "❓"
 __examples__ = {
     example_text_input: [text_input_qs],
@@ -84,21 +105,3 @@ __author__ = "Alexander martin"
 __github_repo__ = "Asaurus1/streamlit-qs"
 __pypi_name__ = "streamlit-qs"
 __package_name__ = "streamlit_qs"
-
-# Wrap functions AFTER examples are linked otherwise it thinks they're a tuple for some reason
-from .. import extra
-
-selectbox_qs = extra(selectbox_qs),
-multiselect_qs = extra(multiselect_qs),
-radio_qs = extra(radio_qs),
-text_area_qs = extra(text_area_qs),
-text_input_qs = extra(text_input_qs),
-number_input_qs = extra(number_input_qs),
-checkbox_qs = extra(checkbox_qs),
-make_query_string = extra(make_query_string),
-update_qs_callback = extra(update_qs_callback),
-add_qs_callback = extra(add_qs_callback),
-clear_qs_callback = extra(clear_qs_callback),
-unenumifier = extra(unenumifier),
-from_query_args = extra(from_query_args),
-from_query_args_index = extra(from_query_args_index),
