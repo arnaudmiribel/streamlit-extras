7c85b55760bb3c8a788aff44eb81a7a871a34f03
Arnaud <arnaudmiribel@gmail.com>
2023-10-18 14:41:31 +0200

    Linting

--- gallery/streamlit_app.py
+++ gallery/streamlit_app.py
@@ -1,10 +1,11 @@
-from importlib import import_module
 import itertools
 import pkgutil
+from importlib import import_module
+
 import streamlit as st
 import streamlit_extras
-from streamlit_extras.row import row
 from streamlit_extras.add_vertical_space import add_vertical_space
+from streamlit_extras.row import row
 
 st.set_page_config(layout="centered")
 
@@ -13,9 +14,13 @@ st.set_page_config(layout="centered")
 
 @st.cache_resource
 def show_extras():
-    extra_names = [extra.name for extra in pkgutil.iter_modules(streamlit_extras.__path__)]
+    extra_names = [
+        extra.name for extra in pkgutil.iter_modules(streamlit_extras.__path__)
+    ]
     num_columns = 12
-    columns = [st.columns(num_columns) for _ in range(len(extra_names) // num_columns + 1)]
+    columns = [
+        st.columns(num_columns) for _ in range(len(extra_names) // num_columns + 1)
+    ]
     columns = itertools.chain.from_iterable(columns)
 
     for extra_name in extra_names:
