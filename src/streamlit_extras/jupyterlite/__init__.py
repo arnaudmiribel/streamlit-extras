8e22c7228c46d337b912a96ccc588911881d5bd5
Rjchauhan18 <rjchauhan5000000@gmail.com>
2023-10-23 21:56:18 +0530

    Added jupyterlite

---
+++ src/streamlit_extras/jupyterlite/__init__.py
@@ -0,0 +1,21 @@
+from streamlit.components.v1 import iframe
+
+from .. import extra
+
+@extra
+def jupyterlite(height:int):
+
+    iframe(
+        src="https://jupyterlite.github.io/demo/repl/index.html?kernel=python&toolbar=1", 
+        height=height
+    )
+
+def example():
+    jupyterlite(1500)
+    
+__title__ = "Jupyterlite"  # title of your extra!
+__desc__ = "Add jupiterlite to your Streamlit app in one command"  # description of your extra!
+__icon__ = "💡"  # give your extra an icon!
+__examples__ = [example]  # create some examples to show how cool your extra is!
+__author__ = "Rahul Chauhan"
+__experimental_playground__ = False  # Optional
