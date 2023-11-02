87bae186e7ed16d98e3f2bb31e698014a241a7ff
Arnaud <arnaudmiribel@gmail.com>
2023-11-02 13:26:26 +0100

    Update src/streamlit_extras/jupyterlite/__init__.py

--- src/streamlit_extras/jupyterlite/__init__.py
+++ src/streamlit_extras/jupyterlite/__init__.py
@@ -14,9 +14,9 @@ def jupyterlite(height:int,width:int):
 def example():
     jupyterlite(1500,1600)
 
-__title__ = "Jupyterlite"  # title of your extra!
+__title__ = "Jupyterlite"
 __desc__ = "Add a Jupyterlite sandbox to your Streamlit app in one command" 
-__icon__ = "💡"  # give your extra an icon!
-__examples__ = [example]  # create some examples to show how cool your extra is!
+__icon__ = "💡"
+__examples__ = [example]
 __author__ = "Rahul Chauhan"
-__experimental_playground__ = False  # Optional
+__experimental_playground__ = False
