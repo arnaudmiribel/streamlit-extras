d0c0330929f8026cf981c0dfaab98304a7bab0d4
Rjchauhan18 <rjchauhan5000000@gmail.com>
2023-11-02 18:38:07 +0530

    removed comments, Updated desc and fixed linting

--- src/streamlit_extras/jupyterlite/__init__.py
+++ src/streamlit_extras/jupyterlite/__init__.py
@@ -2,20 +2,22 @@ from streamlit.components.v1 import iframe
 
 from .. import extra
 
-@extra
-def jupyterlite(height:int,width:int):
 
+@extra
+def jupyterlite(height: int, width: int):
     iframe(
-        src="https://jupyterlite.github.io/demo/repl/index.html?kernel=python&toolbar=1", 
+        src="https://jupyterlite.github.io/demo/repl/index.html?kernel=python&toolbar=1",
         height=height,
-        width=width
+        width=width,
     )
 
+
 def example():
-    jupyterlite(1500,1600)
+    jupyterlite(1500, 1600)
+
 
 __title__ = "Jupyterlite"
-__desc__ = "Add a Jupyterlite sandbox to your Streamlit app in one command" 
+__desc__ = "Add a Jupyterlite sandbox to your Streamlit app in one command"
 __icon__ = "💡"
 __examples__ = [example]
 __author__ = "Rahul Chauhan"
