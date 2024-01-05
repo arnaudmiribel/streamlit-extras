4b461b334198227eecff69820e33bd2fe030609f
Joshua Carroll <joshua.carroll@snowflake.com>
2024-01-05 13:41:04 -0800

    Cleanup and simplification!

--- src/streamlit_extras/streamlit_prometheus/__init__.py
+++ src/streamlit_extras/streamlit_prometheus/__init__.py
@@ -1,27 +1,33 @@
-from typing import Dict, List, NamedTuple, Optional
+from typing import List, NamedTuple
 
 from prometheus_client import CollectorRegistry
-from prometheus_client.registry import Collector
-from prometheus_client.utils import floatToGoString
+from prometheus_client.openmetrics.exposition import generate_latest
 from streamlit.runtime.stats import CacheStatsProvider
 
 from .. import extra
 
 
 class CustomStat(NamedTuple):
-    name: str = ""
-    labelstr: str = ""
-    value: float = 0
-    metadata_str: str = ""
+    metric_str: str = ""
 
     def to_metric_str(self) -> str:
-        if self.metadata_str:
-            return self.metadata_str
-        return "{}{} {}".format(
-            self.name,
-            self.labelstr,
-            floatToGoString(self.value),
-        )
+        return self.metric_str
+
+    def marshall_metric_proto(self, metric) -> None:
+        """Custom OpenMetrics collected via protobuf is not currently supported."""
+        # Included to be compatible with the RequestHandler's _stats_to_proto() method:
+        # https://github.com/streamlit/streamlit/blob/1.29.0/lib/streamlit/web/server/stats_request_handler.py#L73
+        # Fill in dummy values so protobuf format isn't broken
+        label = metric.labels.add()
+        label.name = "cache_type"
+        label.value = "custom_metrics"
+
+        label = metric.labels.add()
+        label.name = "cache"
+        label.value = "not_implemented"
+
+        metric_point = metric.metric_points.add()
+        metric_point.gauge_value.int_value = 0
 
 
 class PrometheusMetricsProvider(CacheStatsProvider):
@@ -29,79 +35,16 @@ class PrometheusMetricsProvider(CacheStatsProvider):
         self.registry = registry
 
     def get_stats(self) -> List[CustomStat]:
-        output = []
-        # This code is taken almost verbatim from
-        # https://github.com/prometheus/client_python/blob/34553ad4cf606d96085e4094c283ba46a2a0f764/prometheus_client/openmetrics/exposition.py#L18
-        # TODO: Figure out if there's a cleaner way to handle this
-        for metric in self.registry.collect():
-            try:
-                mname = metric.name
-                # [JC] Should type come before help? I swapped these to match what Streamlit does but I'm not sure
-                output.append(CustomStat(metadata_str=f"# TYPE {mname} {metric.type}"))
-                output.append(
-                    CustomStat(
-                        metadata_str="# HELP {} {}".format(
-                            mname,
-                            metric.documentation.replace("\\", r"\\")
-                            .replace("\n", r"\n")
-                            .replace('"', r"\""),
-                        )
-                    )
-                )
-                if metric.unit:
-                    output.append(
-                        CustomStat(metadata_str=f"# UNIT {mname} {metric.unit}")
-                    )
-                for s in metric.samples:
-                    if s.labels:
-                        labelstr = "{{{0}}}".format(
-                            ",".join(
-                                [
-                                    '{}="{}"'.format(
-                                        k,
-                                        v.replace("\\", r"\\")
-                                        .replace("\n", r"\n")
-                                        .replace('"', r"\""),
-                                    )
-                                    for k, v in sorted(s.labels.items())
-                                ]
-                            )
-                        )
-                    else:
-                        labelstr = ""
-                    output.append(
-                        CustomStat(
-                            name=s.name,
-                            labelstr=labelstr,
-                            value=s.value,
-                        )
-                    )
-            except Exception as exception:
-                exception.args = (exception.args or ("",)) + (metric,)
-                raise
-        return output
-
-
-class StreamlitCollectorRegistry(CollectorRegistry):
-    """Streamlit specific CollectorRegistry that handles the multiple-rerun behavior for registering metric collectors."""
-
-    # TODO: Note the class currently has a bug where metrics created in the cache will stop working if the cache is cleared
-    #       Figure out if there's a way to fix / merge?
-    def __init__(
-        self, auto_describe: bool = False, target_info: Optional[Dict[str, str]] = None
-    ):
-        super().__init__(auto_describe=auto_describe, target_info=target_info)
-
-    def register(self, collector: Collector) -> None:
-        """Register the collector, ensuring it only gets registered once with Streamlit's execution model"""
-        # TODO: Figure out a cleaner way, that also handles case where other args have changed
-        if collector._get_metric().name in self._names_to_collectors.keys():
-            return
-        return super().register(collector)
+        DUPLICATE_SUFFIX = "\n# EOF\n"
+        output_str = generate_latest(self.registry).decode(encoding="utf-8")
+        if not output_str.endswith(DUPLICATE_SUFFIX):
+            raise ValueError("Unexpected output from OpenMetrics text encoding")
+        output = CustomStat(metric_str=output_str[: -len(DUPLICATE_SUFFIX)])
+        return [output]
 
 
 @extra
-def streamlit_registry() -> StreamlitCollectorRegistry:
+def streamlit_registry() -> CollectorRegistry:
     """
     Expose Prometheus metrics (https://prometheus.io) from your Streamlit app.
 
@@ -112,8 +55,9 @@ def streamlit_registry() -> StreamlitCollectorRegistry:
     https://prometheus.github.io/client_python/
 
     Note: To produce accurate metrics, you are responsible to ensure that unique metric
-    objects are shared across app runs and sessions. We recommend initializing metrics
-    in a cached function as shown in the example below.
+    objects are shared across app runs and sessions. We recommend either 1) initialize
+    metrics in a separate file and import them in the main app script, or 2) initialize
+    metrics in a cached function (and ensure the cache is not cleared during execution).
 
     For an app running locally you can view the output with
     `curl localhost:8501/_stcore/metrics` or equivalent.
@@ -122,16 +66,16 @@ def streamlit_registry() -> StreamlitCollectorRegistry:
 
     stats = Runtime.instance().stats_mgr
 
-    # Did we already register it in another session?
+    # Did we already register it elsewhere? If so, return that copy
     for prv in stats._cache_stats_providers:
         if isinstance(prv, PrometheusMetricsProvider):
             return prv.registry
 
-    # This is the first session on the server, create and register
-    registry = StreamlitCollectorRegistry(auto_describe=True)
+    # This is the function was called, so create the registry
+    # and hook it into Streamlit stats
+    registry = CollectorRegistry(auto_describe=True)
     prv = PrometheusMetricsProvider(registry=registry)
     stats.register_provider(prv)
-    print("Initialized custom metrics provider")
     return registry
 
 
@@ -156,15 +100,17 @@ def example():
     if st.button("Submit"):
         SLIDER_COUNT.labels(app_name).inc(latest)
 
-    """
-    ### Example output at `{host:port}/_stcore/metrics`
-    ```
-    # TYPE my_counter counter
-    # HELP my_counter A cool counter
-    my_counter_total{app_name="prometheus_app"} 14.0
-    my_counter_created{app_name="prometheus_app"} 1.7042185907557938e+09
-    ```
-    """
+    st.write(
+        """
+        ### Example output at `{host:port}/_stcore/metrics`
+        ```
+        # TYPE my_counter counter
+        # HELP my_counter A cool counter
+        my_counter_total{app_name="prometheus_app"} 14.0
+        my_counter_created{app_name="prometheus_app"} 1.7042185907557938e+09
+        ```
+        """
+    )
 
 
 __title__ = "Streamlit Prometheus"
