aac3eab27ebce54fad445f2d6ffb64aae6b95c3e
Arnaud 🎈 <arnaudmiribel@gmail.com>
2024-02-05 13:03:59 +0100

    Update gallery to use pills

--- gallery/streamlit_app.py
+++ gallery/streamlit_app.py
@@ -7,53 +7,92 @@ import streamlit as st
 import streamlit_extras
 from stlite_sandbox import stlite_sandbox
 from streamlit_extras.add_vertical_space import add_vertical_space
-from streamlit_extras.row import row
+from streamlit_pills import pills
 
-st.set_page_config(layout="wide", page_icon=":knot:", page_title="streamlit-extras")
+st.set_page_config(
+    layout="wide",
+    page_icon=":knot:",
+    page_title="streamlit-extras",
+)
 
 
-def centered():
-    _, container, _ = st.columns([1, 3, 1])
+def body():
+    _, container, _ = st.columns([1, 5, 1])
     return container
 
 
-with centered():
+with body():
     """# :knot: streamlit-extras"""
 
-    st.markdown(
-        "streamlit-extras is a Python library putting together useful Streamlit bits of code. It"
-        " includes > 40 (count emojis below!) functional or visual additions to Streamlit that will"
-        " make your life easier or your apps nicer. We call them *extras* and anyone's welcome to add"
-        " their owns!"
-    )
+    left, right = st.columns(2, gap="large")
+
+    with left:
+        st.subheader("Summary", divider=True)
+        st.markdown(
+            "streamlit-extras is a Python library putting together useful Streamlit bits of code. It"
+            " includes > 40 (count emojis below!) functional or visual additions to Streamlit that will"
+            " make your life easier or your apps nicer. "
+            " We call them *extras* and anyone's welcome to add"
+            " their owns!"
+        )
+
+    with right:
+        st.subheader("Get started!", divider=True)
+        st.code("pip install streamlit-extras")
+        st.link_button(
+            "📖  Visit our documentation",
+            "https://arnaudmiribel.github.io/streamlit-extras/",
+            use_container_width=True,
+        )
+        st.link_button(
+            "🐙  Visit our repository",
+            "https://github.com/arnaudmiribel/streamlit-extras",
+            use_container_width=True,
+        )
 
     add_vertical_space(1)
+    st.subheader("Playground", divider=True)
+    st.markdown(
+        """Choose an extra and play with it in the playground below! Powered
+                by [stlite-dynamic-sandbox](https://github.com/blackary/stlite-dynamic-sandbox),
+                a Streamlit component to run [stlite](https://github.com/whitphx/stlite) in Streamlit apps!"""
+    )
 
-    extra_names = [
-        extra.name for extra in pkgutil.iter_modules(streamlit_extras.__path__)
-    ]
+    with st.container(border=True):
 
-    icon_row = row(10)
-    buttons = list()
-    extras = list()
+        extras_metadata = list()
+        for extra in pkgutil.iter_modules(streamlit_extras.__path__):
+            module = import_module(f"streamlit_extras.{extra.name}")
+            metadata = {
+                "module": module,
+                "name": extra.name,
+                "title": module.__title__,
+                "icon": module.__icon__,
+                "examples": module.__examples__,
+                "playground": getattr(module, "__playground__", False),
+            }
 
-    for extra_name in extra_names:
-        mod = import_module(f"streamlit_extras.{extra_name}")
-        icon = mod.__icon__
-        buttons.append(
-            icon_row.button(
-                icon,
-                f"https://arnaudmiribel.github.io/streamlit-extras/extras/{extra_name}/",
-                help=mod.__title__,
-                use_container_width=True,
-            )
+            metadata["label"] = f"{metadata['icon']}  {metadata['title']}"
+            extras_metadata.append(metadata)
+
+        st.caption("Choose extra:")
+        selected = pills(
+            "Select an extra",
+            [extra["label"] for extra in extras_metadata],
+            label_visibility="collapsed",
         )
-        extras.append(mod)
 
-    icon_row.markdown("### ...")
+        extras_modules = list
 
 
 DEFAULT_CODE = """st.balloons()"""
+EXTRA_DOCS_URL = ""
+FALLBACK_CODE = '''st.warning("""Can't use the playground with **{extra_label}**
+extra unfortunately. But you should go and visit its
+[documentation](https://arnaudmiribel.github.io/streamlit-extras/extras/{extra_name}/)
+to learn more about it!
+""")
+'''
 
 if "code" not in st.session_state:
     st.session_state["code"] = DEFAULT_CODE
@@ -61,81 +100,51 @@ if "code" not in st.session_state:
 if "requirements" not in st.session_state:
     st.session_state["requirements"] = ["streamlit", "streamlit-extras"]
 
-button_hit = any(buttons)
 
-""" # Playground """
+extra = [x for x in extras_metadata if x["label"] == selected][0]
 
-if button_hit:
-    extra_name = extra_names[buttons.index(True)]
-    extra = extras[extra_names.index(extra_name)]
-    if extra.__stlite__:
-        example = extra.__examples__[0]
-        if hasattr(example, "__pypi_name__"):
-            if example.__pypi_name__ not in st.session_state["requirements"]:
-                st.session_state["requirements"] += [example.__pypi_name__]
-        example_code = inspect.getsource(example)
-        example_name = example.__name__
-        st.session_state["code"] = f"from streamlit_extras.{extra_name} import *\n\n"
-        st.session_state["code"] += textwrap.dedent(
-            "\n".join(example_code.splitlines()[1:])
-        )
-    else:
-        st.session_state["code"] = DEFAULT_CODE
+if extra["playground"]:
 
-    st.session_state["extra_name"] = extra_name
-    st.session_state["extra_title"] = f"{extra.__icon__} {extra.__title__}"
+    # Get the first extra example metadata
+    if isinstance(extra["examples"], list):
+        example = extra["examples"][0]
+    elif isinstance(extra["examples"], dict):
+        example = extra["examples"][list(extra["examples"].keys())[0]][0]
 
+    example_code = inspect.getsource(example)
+    example_name = example.__name__
 
-if "extra_name" in st.session_state:
-    extra_docs_url = f"https://arnaudmiribel.github.io/streamlit-extras/extras/{st.session_state.extra_name}/"
-    st.caption(
-        f"Trying out **{st.session_state.extra_title}** extra... Learn more in the [docs]({extra_docs_url})!"
+    # If extra is a PyPI package, then add it to the requirements.txt
+    if hasattr(example, "__pypi_name__"):
+        if example.__pypi_name__ not in st.session_state["requirements"]:
+            st.session_state["requirements"] += [example.__pypi_name__]
+
+    st.session_state["code"] = f"from streamlit_extras.{extra['name']} import *\n\n"
+    st.session_state["code"] += textwrap.dedent(
+        "\n".join(example_code.splitlines()[1:])
     )
 
-stlite_sandbox(
-    "import streamlit as st\n\n" + st.session_state.code,
-    height=800,
-    editor=True,
-    requirements=st.session_state.requirements,
-)
-
-
-with centered():
-    add_vertical_space(1)
-    st.markdown(
-        """
-    #### Get started
-    ```
-    pip install streamlit-extras
-    ```
-    """
+else:
+    st.session_state["code"] = FALLBACK_CODE.format(
+        extra_label=extra["label"], extra_name=extra["name"]
     )
 
-    add_vertical_space(1)
-    st.markdown("#### Learn more")
-    links_row = row(2, vertical_align="center")
-    links_row.link_button(
-        "📖  Visit our documentation",
-        "https://arnaudmiribel.github.io/streamlit-extras/",
-        use_container_width=True,
+st.session_state["extra_name"] = extra["name"]
+st.session_state["extra_title"] = extra["label"]
+
+with body():
+
+    if "extra_name" in st.session_state:
+        extra_docs_url = f"https://arnaudmiribel.github.io/streamlit-extras/extras/{st.session_state.extra_name}/"
+
+    PREFIX_CODE = """import streamlit as st
+
+"""
+
+    stlite_sandbox(
+        PREFIX_CODE + st.session_state.code,
+        height=400,
+        editor=True,
+        requirements=st.session_state.requirements,
+        scrollable=True,
     )
-    links_row.link_button(
-        "🐙  Visit our repository",
-        "https://github.com/arnaudmiribel/streamlit-extras",
-        use_container_width=True,
-    )
-
-
-# import streamlit as st
-
-# from streamlit_extras.add_vertical_space import *
-
-# """# 👽 Add Vertical Space"""
-
-# """**Docstring**"""
-# st.help(add_vertical_space)
-
-# """**Example**"""
-# add_n_lines = st.slider("Add n vertical lines below this", 1, 20, 5)
-# add_vertical_space(add_n_lines)
-# st.write("Here is text after the nth line!")
