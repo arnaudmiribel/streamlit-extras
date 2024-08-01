6c1d6b719240bae97c64df71dee32fcbfa266eed
Arnaud <arnaudmiribel@gmail.com>
2024-08-01 09:32:49 +0000

    Add image selector

---
+++ src/streamlit_extras/image_selector/__init__.py
@@ -0,0 +1,122 @@
+from dataclasses import dataclass
+from io import BytesIO
+from typing import Literal
+
+import numpy as np
+import plotly.graph_objects as go
+import requests
+import streamlit as st
+from PIL import ImageDraw
+from PIL.Image import Image
+
+from streamlit_extras import extra
+
+
+@dataclass
+class ImageSelector:
+    image: Image
+    selection_type: Literal["lasso", "box"]
+
+    @property
+    def image_array(self):
+        return np.array(self.image)
+
+    @extra
+    def show(self, width: int = 300, height: int = 300):
+
+        fig = go.Figure().add_trace(go.Image(z=self.image))
+
+        if self.selection_type == "lasso":
+            dragmode = "lasso"
+        elif self.selection_type == "box":
+            dragmode = "select"
+
+        fig.update_layout(
+            dragmode=dragmode,
+            xaxis=dict(showticklabels=False),  # hide x-axis ticks
+            yaxis=dict(showticklabels=False),  # hide y-axis ticks
+            margin=dict(
+                t=0,
+                b=5,
+            ),
+            width=width,
+            height=height,
+        )
+
+        config = {
+            "displaylogo": False,
+            "displayModeBar": False,
+        }
+
+        self.selection = st.plotly_chart(fig, on_select="rerun", config=config)
+        return self.selection
+
+    @extra
+    def show_selection(self):
+        if coordinates := self.selection["selection"].get("box"):
+            x_min, x_max = coordinates[0]["x"]
+            y_min, y_max = coordinates[0]["y"]
+
+            selection_img = self.image_array[
+                int(y_min) : int(y_max), int(x_min) : int(x_max)
+            ]
+            st.image(selection_img)
+
+        elif coordinates := self.selection["selection"].get("lasso"):
+            lasso_x, lasso_y = coordinates[0]["x"], coordinates[0]["y"]
+            # Create a white background image
+
+            img = self.image_array
+            white_background = np.ones_like(self.image_array) * 255
+
+            # Convert image and coordinates to PIL
+            img_pil = Image.fromarray((img).astype(np.uint8))
+            mask = Image.new("L", (img.shape[1], img.shape[0]), 0)
+            draw = ImageDraw.Draw(mask)
+            polygon = list(zip(lasso_x, lasso_y))
+            draw.polygon(polygon, outline=1, fill=1)
+            mask = np.array(mask)
+
+            # Extract the pixels within the lasso selection
+            selected_pixels = np.array(img_pil)
+            white_background[mask == 1] = selected_pixels[mask == 1]
+
+            # Extract the bounding box of the polygon
+            min_x, min_y = int(min(lasso_x)), int(min(lasso_y))
+            max_x, max_y = int(max(lasso_x)), int(max(lasso_y))
+            selection_img = Image.fromarray(
+                white_background.astype(np.uint8)[min_y:max_y, min_x:max_x]
+            )
+
+            # Display the result using Streamlit
+            st.image(selection_img)
+
+
+def example():
+    response = requests.get(
+        "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    )
+
+    img: Image = Image.open(BytesIO(response.content))
+
+    selection_type = st.radio(
+        "Selection type", ["lasso", "box"], index=0, horizontal=True
+    )
+
+    image = ImageSelector(image=img, selection_type=selection_type)
+    image.show()
+
+    if image.selection:
+        st.json(image.selection, expanded=False)
+        image.show_selection()
+
+
+__title__ = "Image Selector"
+__desc__ = """
+Allows users to select an area within an image, using a lasso or a bounding
+box."""
+__icon__ = "🤠"
+__examples__ = [example]
+__author__ = "Arnaud Miribel"
+__experimental_playground__ = False
+__stlite__ = True
